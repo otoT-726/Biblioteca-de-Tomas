5f5fb09e5c7d9e424072077911f827f02f363314
Tomas Jaureguiberry <jaureguiberry.tomas@gmail.com>
2025-03-17 16:57:02 -0300

    Implementacion base de mi generador

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('generador de claves ')
+import string
+import random
+
+print('Generador de claves! ')
+
+chars = string.ascii_letters + string.digits + string.punctuation
+leng = 10
+password = ''
+
+for _ in range(leng):
+    password = password + random.choice(chars)
+
+print('La clave generada es: ', password)
